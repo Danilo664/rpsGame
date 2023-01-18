97df5d6963bef65145e7339076cb5e540529061b
Danilo664 <danilomilosevic123@gmail.com>
2023-01-18 18:30:43 +0100

    almost functioning game with UNDEFINED additional message to be fixed

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const rpsSigns = ["rock", "paper", "scissors"];
 let signChoice = Math.floor(Math.random()*rpsSigns.length);
-let sign = rpsSigns[signChoice];
+let computerSelection = rpsSigns[signChoice];
 
-function getComputerChoice(){
-    return sign;
+
+let playerSelection = prompt ("Choose!").toLowerCase();
+
+alert (playRound())
+
+function playRound(){
+
+
+    while(playerSelection!="rock" && playerSelection!="scissors" && playerSelection!="paper"){
+        alert("You didn't choose valid option, choose again!");
+        playerSelection = prompt ("Choose!").toLowerCase();
+    }
+    
+    alert("You choose " + playerSelection);
+    alert("Computer chooses " + computerSelection);
+
+    if(playerSelection==="rock"){
+        if(computerSelection==="rock"){alert("It's a tie!")}
+        else if(computerSelection==="paper"){alert("You lose! Paper beats rock!")}
+        else if(computerSelection==="scissors"){alert("You win! Rock beats Scisors") }
+    }
+    else if(playerSelection==="paper"){
+        if(computerSelection==="rock"){alert("You win! Paper beats rock!")}
+        else if(computerSelection==="paper"){alert("It's a tie!")}
+        else if(computerSelection==="scissors"){alert("You lose! Scissors beat paper!")}
+    }
+    else if(playerSelection==="scissors"){
+        if(computerSelection==="rock"){alert("You lose! Rock beats Scisors!")}
+        else if(computerSelection==="paper"){alert("You win! Scissors beat paper!")}
+        else if(computerSelection==="scissors"){alert("It's a tie!")}
+    }
 }
-
-
-
-
-
-
-
-//alert(getComputerChoice())
