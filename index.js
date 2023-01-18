8acefb6986b73477f9eab18adc5c397a9ef0e115
Danilo664 <danilomilosevic123@gmail.com>
2023-01-18 17:01:13 +0100

    Index added just 1 blank row, but js file got computerchoice function working

---
+++ index.js
@@ -0,0 +1,15 @@
+const rpsSigns = ["rock", "paper", "scissors"];
+let signChoice = Math.floor(Math.random()*rpsSigns.length);
+let sign = rpsSigns[signChoice];
+
+function getComputerChoice(){
+    return sign;
+}
+
+
+
+
+
+
+
+//alert(getComputerChoice())
