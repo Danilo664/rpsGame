6b1b614e995235bc1f83ae71f3dfc642d15d7343
Danilo664 <danilomilosevic123@gmail.com>
2023-01-19 16:43:19 +0100

    fully functional game of rockPaperScissors , can be run trough console ONLY, for now, use game() to start

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const rpsSigns = ["rock", "paper", "scissors"];
-let signChoice = Math.floor(Math.random()*rpsSigns.length);
-let computerSelection = rpsSigns[signChoice];
+let signChoice;
+let computerSelection;
 
+let playerSelection="";
 
-let playerSelection = prompt ("Choose!").toLowerCase();
+let player=0;
+let computer=0;
+
+function playRound(){
+    signChoice = Math.floor(Math.random()*rpsSigns.length);
+    playerSelection = prompt ("Choose!").toLowerCase();
+    computerSelection = rpsSigns[signChoice];
 
 while(playerSelection!="rock" && playerSelection!="scissors" && playerSelection!="paper"){
         alert("You didn't choose valid option, choose again!");
@@ -14,17 +21,34 @@ while(playerSelection!="rock" && playerSelection!="scissors" && playerSelection!
     alert("Computer chooses " + computerSelection);
 
     if(playerSelection==="rock"){
-        if(computerSelection==="rock"){alert("It's a tie!")}
-        else if(computerSelection==="paper"){alert("You lose! Paper beats rock!")}
-        else if(computerSelection==="scissors"){alert("You win! Rock beats Scisors")}
+        if(computerSelection==="rock"){return "It's a tie!";}
+        else if(computerSelection==="paper"){computer++;return"You lose! Paper beats rock!"; }
+        else if(computerSelection==="scissors"){player++;return"You win! Rock beats Scisors"; }
     }
     else if(playerSelection==="paper"){
-        if(computerSelection==="rock"){alert("You win! Paper beats rock!")}
-        else if(computerSelection==="paper"){alert("It's a tie!")}
-        else if(computerSelection==="scissors"){alert("You lose! Scissors beat paper!")}
+        if(computerSelection==="rock"){player++;return"You win! Paper beats rock!"; }
+        else if(computerSelection==="paper"){return("It's a tie!");}
+        else if(computerSelection==="scissors"){computer++;return"You lose! Scissors beat paper!"; }
     }
     else if(playerSelection==="scissors"){
-        if(computerSelection==="rock"){alert("You lose! Rock beats Scisors!")}
-        else if(computerSelection==="paper"){alert("You win! Scissors beat paper!")}
-        else if(computerSelection==="scissors"){alert("It's a tie!")}
+        if(computerSelection==="rock"){computer++;return"You lose! Rock beats Scisors!"; }
+        else if(computerSelection==="paper"){player++;return"You win! Scissors beat paper!"; }
+        else if(computerSelection==="scissors"){return("It's a tie!");}
     }
+}
+
+
+function game(){
+    for (i=1; i<5; i++){
+        console.log("Player: " + player + "   " + "Computer: " + computer)
+        alert(playRound())
+    }
+
+    if(player>computer){
+        return "You WIN with " + player + " points!";
+    }
+    else if(computer>player){
+        return "You lose! Computer has " + computer + " points!";
+    }
+    else{return "It's a tie! You have " + player + " points, and computer has " + computer + " points ."}
+}
