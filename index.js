aa0d5716736120bdc6f08ec57fe5a3c052ce0d12
Danilo664 <danilomilosevic123@gmail.com>
2023-02-20 16:58:17 +0100

    reworked original game js file, due to UI, now its much better!

--- index.js
+++ index.js
@@ -4,51 +4,60 @@ let computerSelection;
 
 let playerSelection="";
 
-let player=0;
-let computer=0;
+let playerPoints= 0;
+let computerPoints= 0;
 
-function playRound(){
+let player= document.getElementById("player")
+player.innerText = playerPoints;
+let computer= document.getElementById("computer")
+computer.innerText = computerPoints;
+let outcomeTxt= document.getElementById("outcomeTxt")
+outcomeTxt.innerText = "";
+
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
+rock.addEventListener("click", ()=>{
+    playerSelection= "rock";
+    playGame();
+});
+paper.addEventListener("click", ()=>{
+    playerSelection= "paper";
+    playGame();
+});
+scissors.addEventListener("click", ()=>{
+    playerSelection= "scissors";
+    playGame();
+});
+
+function playGame(){
     signChoice = Math.floor(Math.random()*rpsSigns.length);
-    playerSelection = prompt ("Choose!").toLowerCase();
     computerSelection = rpsSigns[signChoice];
 
-while(playerSelection!="rock" && playerSelection!="scissors" && playerSelection!="paper"){
-        alert("You didn't choose valid option, choose again!");
-        playerSelection = prompt ("Choose!").toLowerCase();
-    }
-    
-    alert("You choose " + playerSelection);
-    alert("Computer chooses " + computerSelection);
-
     if(playerSelection==="rock"){
-        if(computerSelection==="rock"){return "It's a tie!";}
-        else if(computerSelection==="paper"){computer++;return"You lose! Paper beats rock!"; }
-        else if(computerSelection==="scissors"){player++;return"You win! Rock beats Scisors"; }
+        if(computerSelection==="rock"){outcomeTxt.innerText="It's a tie";}
+        else if(computerSelection==="paper"){ outcomeTxt.innerText="Ouch, paper beats rock!" ;computerPoints++;cS();}
+        else if(computerSelection==="scissors"){ outcomeTxt.innerText="YES! Rock beats scissors!" ;playerPoints++;cS();}
     }
     else if(playerSelection==="paper"){
-        if(computerSelection==="rock"){player++;return"You win! Paper beats rock!"; }
-        else if(computerSelection==="paper"){return("It's a tie!");}
-        else if(computerSelection==="scissors"){computer++;return"You lose! Scissors beat paper!"; }
+        if(computerSelection==="rock"){outcomeTxt.innerText="YES! Paper beats rock!" ;playerPoints++;cS();}
+        else if(computerSelection==="paper"){outcomeTxt.innerText="It's a tie";}
+        else if(computerSelection==="scissors"){outcomeTxt.innerText="Ouch, scissors beat paper!" ;computerPoints++; cS();}
     }
     else if(playerSelection==="scissors"){
-        if(computerSelection==="rock"){computer++;return"You lose! Rock beats Scisors!"; }
-        else if(computerSelection==="paper"){player++;return"You win! Scissors beat paper!"; }
-        else if(computerSelection==="scissors"){return("It's a tie!");}
+        if(computerSelection==="rock"){outcomeTxt.innerText="Ouch, rock beat scissors!" ;computerPoints++; cS();}
+        else if(computerSelection==="paper"){outcomeTxt.innerText="YES! Scissors beat paper!" ;playerPoints++; cS();}
+        else if(computerSelection==="scissors"){outcomeTxt.innerText="It's a tie";}
     }
+    computer.innerText = computerPoints;
+    player.innerText = playerPoints;
 }
-
-
-function game(){
-    for (i=1; i<5; i++){
-        console.log("Player: " + player + "   " + "Computer: " + computer)
-        alert(playRound())
+function cS(){
+    if(playerPoints==5){
+        outcomeTxt.innerText="You WIN! You managed to get 5 points!";
     }
-
-    if(player>computer){
-        return "You WIN with " + player + " points!";
+    else if(computerPoints==5){
+        outcomeTxt.innerText="You LOSE! Computer managed to get 5 points!";
     }
-    else if(computer>player){
-        return "You lose! Computer has " + computer + " points!";
-    }
-    else{return "It's a tie! You have " + player + " points, and computer has " + computer + " points ."}
 }
